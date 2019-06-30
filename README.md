60d48f42468cc0659290b1b38725f2153868c99e
Sebastian Simmons <sebastianctsimmons@gmail.com>
2019-06-29 22:10:32 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## Single .m4b Audiobook Script
+
+iTunes and the iOS books app prefer audiobooks as a single m4b file. Unfortunately, many audiobooks comes as multiple mp3 files. This program takes a directory filled with mp3 files and creates a single m4b file out of them.
---
+++ convert-audiobook.py
@@ -0,0 +1,58 @@
+#
+#
+#
+#
+import sys
+import os
+
+from pydub import AudioSegment
+
+def convertToSingleFile(folder):
+    """Converts a list of mp3 files to a single m4b (apple audiobook) format"""
+    path = os.getcwd() + "\\" + folder  # This might be unnecessary
+    files = []
+    loop = 0
+    segments = []
+    output = AudioSegment.empty()
+    # r=root, d=directories, f = files
+    for r, d, f in os.walk(path):
+            for file in f:
+                if file.endswith(".mp3"):
+                    files.append(os.path.join(r, file))
+                    print("LOADING: " +  os.path.join(r, file))
+                    output = output + (AudioSegment.from_mp3(folder + "\\" +file))
+
+
+    big = output.export("output.mp3", format="mp3")
+    #big = output.export("pls.m4b", format="m4b")
+
+    os.system("ffmpeg -i output.mp3 output.m4b\nrm output.mp3")
+
+
+
+def viewAllSubDirs(path):
+    folders = []
+    # r=root, d=directories, f = files
+    for r, d, f in os.walk(path):
+        for folder in d:
+            folders.append(os.path.join(r, folder))
+
+    for f in folders:
+        print(f)
+    
+
+def viewAllFiles(path):
+    files = []
+    # r=root, d=directories, f = files
+    for r, d, f in os.walk(path):
+        for file in f:
+            files.append(os.path.join(r, file))
+
+    for f in files:
+        print(f)
+
+
+
+if __name__ == "__main__":
+    # Create check for proper arguments
+    convertToSingleFile('Influence')
