10013f58ca4a8d9078ce06c6b8398499e7db758d
Sebastian Simmons <sebastianctsimmons@gmail.com>
2020-05-15 15:59:29 -0700

    Added automatic metadata scraping to the multithreaded script

--- convert-audiobook-group-multithreaded.py
+++ convert-audiobook-group-multithreaded.py
@@ -3,35 +3,43 @@ import os
 from multiprocessing import Pool
 
 from pydub import AudioSegment
+from pydub.utils import mediainfo
 
 def checkFiles(directory):
     path = os.getcwd() + "\\" + directory
     files = []
-       # r=root, d=directories, f = files
+    # r=root, d=directories, f = files
     for r, d, f in os.walk(path):
         for file in f:
             if file.endswith(".mp3"):
                 print(os.path.join(r, file))
+                print(mediainfo(os.path.join(r, file)))
 
     return
 
 def worker(info):
         filename = info['filename']
 
-        title = info['title']
-        author = info['author']
+        #title = info['title']
+        #author = info['author']
         outputdir = info['outputdir']
         index = info['index']
         newFile = 'track' + str(index) +".m4b"
 
+        m_data = mediainfo(filename)
+
         ffmpeg = "ffmpeg -i \"" + filename + "\" -vn "
-        ffmpeg += "-metadata track={0}/{1} ".format(index + 1, info['out_of'])
-        ffmpeg += "-metadata album=\"" + title + "\" "
-        ffmpeg += "-metadata title=\"" + title + "\" "
-        ffmpeg += "-metadata author=\"" + author + "\" "
-        ffmpeg += outputdir + "\\" + newFile
+        ffmpeg += "-metadata track={0}/{1} ".format(index, info['out_of'])
+        #ffmpeg += "-metadata album=\"" + title + "\" "
+        #ffmpeg += "-metadata title=\"" + title + "\" "
+        #ffmpeg += "-metadata author=\"" + author + "\" "
+        for data in m_data:
+            ffmpeg += "-metadata " + str(data) + "=\"" + str(m_data[data]) + "\" "
+        ffmpeg += "\""+outputdir + "\\" + newFile + "\""
         print(ffmpeg)
-        os.system(ffmpeg)     
+
+
+        os.system(ffmpeg)
   
 
 if __name__ == "__main__":
@@ -44,9 +52,10 @@ if __name__ == "__main__":
         path = os.getcwd() + "\\" + directory
         files = []
 
-        title = input("Title:\n>")
-        author = input("Author:\n>")
+        #title = input("Title:\n>")
+        #author = input("Author:\n>")
         outputdir = input("dir name:\n>")
+        #outputdir = directory + "-m4b"
         converted_count = 0
 
         # r=root, d=directories, f = files
@@ -55,8 +64,8 @@ if __name__ == "__main__":
                 if file.endswith(".mp3"):
                     converted_count += 1
                     info = {'filename':os.path.join(r, file),
-                            'title':title,
-                            'author':author,
+                            #'title':title,
+                            #'author':author,
                             'outputdir':outputdir,
                             'index':converted_count}
                     files.append(info)
